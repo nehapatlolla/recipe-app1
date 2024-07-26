03c7d6304c51ff08b5f5e54a5a49616499614c4b
nehapatlolla <neha.patlolla@proclink.com>
2024-07-26 15:48:02 +0530

    added nonveg

---
+++ src/app/nonveg/nonveg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nonveg',
+  standalone: true,
+  imports: [],
+  templateUrl: './nonveg.component.html',
+  styleUrl: './nonveg.component.scss'
+})
+export class NonvegComponent {
+
+}

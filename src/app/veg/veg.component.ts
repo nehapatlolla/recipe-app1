09233977cb43484dce2dde63e5ff33172eff96e2
Tejasanala <tejaswini.sanala@proclink.com>
2024-07-26 15:57:39 +0530

    html is added

---
+++ src/app/veg/veg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veg',
+  standalone: true,
+  imports: [],
+  templateUrl: './veg.component.html',
+  styleUrl: './veg.component.scss',
+})
+export class VegComponent {}
